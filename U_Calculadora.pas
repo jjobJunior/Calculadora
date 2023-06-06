bdfb2aeffbab88b3967c3bb9233ef5057baa34f3
jjobJunior <54251905+jjobJunior@users.noreply.github.com>
2023-06-05 22:41:00 -0300

    Estudo de Segunda

--- U_Calculadora.pas
+++ U_Calculadora.pas
@@ -5,26 +5,31 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
-  FMX.Controls.Presentation, FMX.StdCtrls;
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts, FMX.Edit;
 
 type
   TForm1 = class(TForm)
-    Button1: TButton;
-    Button2: TButton;
-    Button3: TButton;
-    Button4: TButton;
-    Button5: TButton;
-    Button6: TButton;
-    Button7: TButton;
-    Button8: TButton;
-    Button9: TButton;
-    Button10: TButton;
-    Button11: TButton;
-    Button12: TButton;
-    Button13: TButton;
-    Button14: TButton;
-    Button15: TButton;
-    Button16: TButton;
+    BtnUm: TButton;
+    BtnDois: TButton;
+    BtnTres: TButton;
+    BtnDividir: TButton;
+    BtnQuatro: TButton;
+    BtnCinco: TButton;
+    BtnSeis: TButton;
+    BtnMultiplicar: TButton;
+    BtnSete: TButton;
+    BtnOito: TButton;
+    BtnNove: TButton;
+    BtnSubtrair: TButton;
+    BtnPonto: TButton;
+    BtnZero: TButton;
+    BtnIgual: TButton;
+    BtnSoma: TButton;
+    Layout1: TLayout;
+    CaixaTopo: TLayout;
+    EditDois: TEdit;
+    EdtiUm: TEdit;
+    Label1: TLabel;
   private
     { Private declarations }
   public
